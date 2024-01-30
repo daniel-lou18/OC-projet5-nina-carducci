2dd7c0ba404a7a1df2c2db0caeeabdcb47411c89
daniel-lou18 <danielderudder@hotmail.com>
2024-01-30 09:16:13 +0100

    Corrections

--- assets/loadScripts.js
+++ assets/loadScripts.js
@@ -10,8 +10,18 @@ function loadScript(src) {
   return new Promise((resolve, reject) => {
     const script = document.createElement("script");
     script.src = src;
+    if (
+      script.src ===
+      "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+    ) {
+      script.crossOrigin = "anonymous";
+      script.integrity =
+        "sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==";
+      script.referrerpolicy = "no-referrer";
+    }
     script.onload = resolve;
-    script.onerror = reject;
+    script.onerror = () =>
+      reject(`Erreur lors du chargement du script : ${src}`);
     document.head.appendChild(script);
   });
 }
